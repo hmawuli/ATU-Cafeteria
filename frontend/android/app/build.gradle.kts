a10988b91c110855ed59ea46fcfce11177f3c75a
doxapalace-droid <doxapalace@gmail.com>
2026-09-20 19:10:51 +0000

    Set production-safe Android application ID

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.atu_cafeteria"
+    namespace = "com.atu.cafeteria"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,8 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.atu_cafeteria"
+        applicationId = "com.atu.cafeteria"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -32,8 +31,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Keep debug signing for local/defense builds until a production keystore is supplied.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
