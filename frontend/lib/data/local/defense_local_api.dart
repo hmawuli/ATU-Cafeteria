7a18a5ad605fc35b53290e725f56e3863130b7c6
doxapalace-droid <doxapalace@gmail.com>
2026-09-23 16:21:04 +0000

    Remove defense-only local API adapter

--- frontend/lib/data/local/defense_local_api.dart
+++
@@ -1,4 +0,0 @@
-// Keep sqflite out of Flutter Web builds. Mobile/desktop use SQLite; web gets
-// an explicit unsupported implementation because defense mode is an APK-only path.
-export 'defense_local_api_stub.dart'
-    if (dart.library.io) 'defense_local_api_io.dart';
