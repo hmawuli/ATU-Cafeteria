c4b280b698a64bc4d30d271906d245fef4708451
doxapalace-droid <doxapalace@gmail.com>
2026-09-24 07:45:17 +0000

    Migrate active order_repository.dart to customer domain

--- frontend/lib/data/repositories/order_repository.dart
+++ frontend/lib/data/repositories/order_repository.dart
@@ -5,7 +5,7 @@ class OrderRepository {
   final ApiClient client;
   OrderRepository(this.client);
   Future<List<Order>> myOrders() async {
-    final data = await client.request('GET', 'student/orders');
+    final data = await client.request('GET', 'customer/orders');
     final list = data is Map ? (data['data'] ?? data['orders'] ?? []) : data;
     return (list as List)
         .map((e) => Order.fromJson(Map<String, dynamic>.from(e)))
